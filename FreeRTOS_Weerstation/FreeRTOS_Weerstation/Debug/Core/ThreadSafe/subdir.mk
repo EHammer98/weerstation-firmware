b4c436170023ee3fb624da534855b58dbc4ecb01
aliciak6 <alicia6.koker@gmail.com>
2021-10-07 11:53:58 +0200

    taak toegveoegd voor de sensor en geprobeerd te debuggen

---
+++ FreeRTOS_Weerstation/FreeRTOS_Weerstation/Debug/Core/ThreadSafe/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/ThreadSafe/newlib_lock_glue.c 
+
+OBJS += \
+./Core/ThreadSafe/newlib_lock_glue.o 
+
+C_DEPS += \
+./Core/ThreadSafe/newlib_lock_glue.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/ThreadSafe/%.o: ../Core/ThreadSafe/%.c Core/ThreadSafe/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F030x8 -DSTM32_THREAD_SAFE_STRATEGY=4 -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM0 -I../Core/ThreadSafe -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
