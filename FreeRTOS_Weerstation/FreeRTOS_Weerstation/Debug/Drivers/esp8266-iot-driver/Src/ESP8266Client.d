8f2747d84e5f388de384cde6521759260d9458c5
aliciak6 <89914517+aliciak6@users.noreply.github.com>
2021-10-07 17:06:16 +0200

    verkeerde plaats

--- FreeRTOS_Weerstation/FreeRTOS_Weerstation/Debug/Drivers/esp8266-iot-driver/Src/ESP8266Client.d
+++
@@ -1,5 +0,0 @@
-Drivers/esp8266-iot-driver/Src/ESP8266Client.o: \
- ../Drivers/esp8266-iot-driver/Src/ESP8266Client.c \
- ../Drivers/esp8266-iot-driver/Inc/ESP8266Client.h
-
-../Drivers/esp8266-iot-driver/Inc/ESP8266Client.h:
