a852c567cb214f528173dcd17181e1da93aecf7a
aliciak6 <alicia6.koker@gmail.com>
2021-10-31 22:16:16 +0100

    incorrect definition of function

--- FreeRTOS_Weerstation/Core/Inc/main.h
+++ FreeRTOS_Weerstation/Core/Inc/main.h
@@ -54,7 +54,7 @@ extern "C" {
 void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
-void connectESPtoWifi();
+int connectESPtoWifi();
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
